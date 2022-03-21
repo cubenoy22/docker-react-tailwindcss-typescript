a8c7d95cc76409a324add0c6558efe10b85a7aa5
cubenoy22 <cubenoy22@gmail.com>
2022-03-21 14:33:32 +0900

    fix: Rename extension text in App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,7 +11,7 @@ const App: React.VFC = () => {
           alt='logo'
         />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className='text-[#61dafb]'
